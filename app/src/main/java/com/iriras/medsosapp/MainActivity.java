39a0a0167d64de387e74eebdbc49f8f2707d9f43
Irfan Irawan Sukirman <irfan.irawan@gits.co.id>
2016-10-18 17:01:20 +0700

    Google Sign-in 70%

--- app/src/main/java/com/iriras/medsosapp/MainActivity.java
+++ app/src/main/java/com/iriras/medsosapp/MainActivity.java
@@ -1,13 +1,47 @@
 package com.iriras.medsosapp;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+
+import id.gits.mvvmcore.activity.GitsActivity;
+
+public class MainActivity extends GitsActivity<MainController> {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        mController.setGoogleConfig();
+    }
+
+    @Override
+    protected int getToolbarId() {
+        return 0;
+    }
+
+    @Override
+    protected int getResLayout() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected MainController createController() {
+        return new MainController();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == mController.RC_SIGN_IN && resultCode == RESULT_OK) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            mController.handleGoogleResult(result);
+        }
     }
 }
